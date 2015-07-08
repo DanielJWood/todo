1053c305721bbef927ef91cd319fd4a4b208b310
Daniel Wood <daniel.j.wood11@gmail.com>
2015-07-08 14:35:59 -0400

    made level 1 responsive on reload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
 // Insert your scripts here
 
+var width = parseInt(d3.select("#master_container").style("width")),
+  height = width / 2,
+  centered;
 
-var width = 960,
-    height = 600,
-    centered;
 
-var projection = d3.geo.albersUsa();
+var projection = d3.geo.albersUsa()
+    .scale(width)
+    .translate([width / 2, ((height / 2))])  ;
 
 var path = d3.geo.path()
     .projection(projection);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#master_container").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-svg.append("rect")
+var rect = svg.append("rect")
     .attr("class", "background")
     .attr("width", width)
     .attr("height", height)
@@ -35,18 +37,20 @@ d3.json("js/statesregion2.json", function(error, regions) {
       .on("click", clicked);
 
 	g.append("path")
-	    .datum(topojson.mesh(regions, regions.objects.regions_usa, function(a, b) { return a !== b && a.id !== "IRL"; }))
+	    .datum(topojson.mesh(regions, regions.objects.regions_usa, function(a, b) { return a !== b}))
 	    .attr("d", path)
 	    .attr("class", "main-boundary");
 
 	g.append("path")
-	    .datum(topojson.mesh(regions, regions.objects.states2, function(a, b) { return a !== b && a.id !== "IRL"; }))
+	    .datum(topojson.mesh(regions, regions.objects.states2, function(a, b) { return a !== b}))
 	    .attr("d", path)
 	    .attr("class", "subunit-boundary");	    
 
 
 });
-function clicked(d) { 
+
+function clicked(d) {
+console.log(d) 
 // D3 stuff on click
   var x, y, k;
 
@@ -72,6 +76,59 @@ function clicked(d) {
       .style("stroke-width", 1.5 / k + "px");
 
 // Add the dom elements.
+}
+
+d3.select(window).on('resize', resize); 
+
+function resize() {   
+
+// console.log(d3.select("subunit-boundary"))
+//   // Do some resize stuff here
+
+//     var width = parseInt(d3.select("#master_container").style("width")),
+//       height = width / 2;
+
+//     var x, y, k;
+//     var k = 0.5;
+//     var x = width / 2;
+//     var y = height / 2;        
+
+  //     // resize projection
+  //     // Smaller viewport
+  //     if (width <= 800) {
+  //       projection
+  //         .scale(width * 1.05)
+  //         .translate([width / 2, ((height / 2) + 45)])             
+  //     } else if (width <= 900) {
+  //       projection
+  //         .scale(width * 1.2)
+  //         .translate([width / 2, ((height / 2) + 30)])  
+  //     } 
+
+  //     // full viewport
+  //     else {
+  //       projection
+  //         .scale(width)
+  //         .translate([width / 2, ((height / 2) + 10)])   
+  //     };      
+
+  // console.log(width)
+
+  // svg.transition().duration(750)
+  //   .attr("width", width)
+  //   .attr("height", height);
+
+  // rect.transition().duration(750)  
+  //     .attr("width", width)
+  //     .attr("height", height);
+
+  // g.transition()
+  //   .duration(750)
+  //   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
+
 
 
 }
+
+
+
